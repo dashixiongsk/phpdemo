33826aa17e9384985bdfcce91c4f8e3671ef433d
sk <2464563830@qq.com>
2018-01-16 09:40:02 +0800

    Update suanfa.php

--- suanfa.php
+++ suanfa.php
@@ -49,3 +49,42 @@ function kuaisu($arr){
 }
 
 var_dump(kuaisu($arr));
+
+
+
+#二分查找算法
+/**********************************
+*1.必须采用顺序存储结构
+*
+*2.必须按关键字大小有序排列。
+**********************************/
+function getVal ($arr, $target) {
+
+	$end = count($arr) - 1;
+	$start = 0;
+
+	while($start <= $end) {
+
+		$mid = floor(($end-$start)/2);
+
+		if ($arr[$mid] == $target) {
+			return $mid;
+		}
+
+		if ($arr[$mid] > $target) {
+			$end = $mid - 1;
+		}
+		if ($arr[$mid] < $target) {
+			$end = $mid + 1;
+		}
+
+	}
+
+	return false;
+}
+
+	$arr = array(1,3,5, 6, 7, 9, 11);
+
+	$index = getVal($arr, 6);
+
+	var_dump($index);
